55b5552bd6f1e581d95a03e5d4d38f84b9b729f6
Janina Mattes <janina.alica.mattes@gmail.com>
2023-10-07 21:25:13 +0200

    src: Add nav highlighting for side- and navbar

--- assets/js/scroll-smooth.js
+++ assets/js/scroll-smooth.js
@@ -1,21 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
   const scrollLinks = document.querySelectorAll("a[data-scroll]");
-  const sidebarLinks = document.querySelectorAll(".sidebar-item");
-  const navbarLinks = document.querySelectorAll(".sidebar-item");
+  const sidebarLinks = document.querySelectorAll(".sidebar-nav a");
+  const navbarLinks = document.querySelectorAll(".navbar-nav a");
+  let previousIndex = localStorage.getItem("previousIndex");
 
-  scrollLinks.forEach(function (link) {
+  scrollLinks.forEach(function (link, index) {
     link.addEventListener("click", function (event) {
       event.preventDefault();
       const targetId = this.getAttribute("href").substring(1);
       const targetElement = document.getElementById(targetId);
 
+      if (previousIndex !== null) {
+        sidebarLinks[previousIndex].classList.remove("selected");
+        navbarLinks[previousIndex].classList.remove("selected");
+      }
+
       if (targetElement) {
         targetElement.scrollIntoView({ behavior: "smooth" });
-        
-        scrollLinks.forEach(function (otherLink) {
-          otherLink.classList.remove("selected");
-        });
-        this.classList.add("selected");
+
+        sidebarLinks[index].classList.add("selected");
+        navbarLinks[index].classList.add("selected");
+
+        previousIndex = index;
+        localStorage.setItem("previousIndex", previousIndex);
       }
     });
   });
