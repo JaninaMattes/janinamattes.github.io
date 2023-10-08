231274b1476689f8ae86dc66368fd08177f45a1e
Janina Mattes <janina.alica.mattes@gmail.com>
2023-10-08 19:06:30 +0200

    src: Add brand visibility on screen change

--- assets/js/navbar-toggle.js
+++ assets/js/navbar-toggle.js
@@ -1,25 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const navbarToggler = document.querySelector(".navbar-toggler");
-    const navbarCollapse = document.querySelector(".navbar-collapse");
-    const navbarBrand = document.querySelector(".navbar-brand");
-  
-    if (navbarToggler) {
-      navbarToggler.addEventListener("click", function () {
-        navbarToggler.classList.toggle("active");
-        if (window.innerWidth <= 767) {
-          // Check if the screen width is less than or equal to 767px
-          navbarBrand.style.visibility = 'hidden';
-        }
-      });
+  const navbarToggler = document.querySelector(".navbar-toggler");
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+  const navbarBrand = document.querySelector(".navbar-brand");
+
+  function updateNavbarBrandVisibility() {
+    if (window.innerWidth <= 767) {
+      // Check if the screen width is less than or equal to 767px
+      navbarBrand.style.visibility = "hidden";
+    } else {
+      navbarBrand.style.visibility = "visible";
     }
-  
-    const navbarLinks = document.querySelectorAll(".navbar-nav .nav-link");
-  
-    navbarLinks.forEach(function (link) {
-      link.addEventListener("click", function () {
-        navbarToggler.classList.remove("active");
-        navbarCollapse.classList.remove("show");
-      });
+  }
+
+  if (navbarToggler) {
+    navbarToggler.addEventListener("click", function () {
+      navbarToggler.classList.toggle("active");
+      updateNavbarBrandVisibility();
+    });
+  }
+
+  const navbarLinks = document.querySelectorAll(".navbar-nav .nav-link");
+
+  navbarLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      navbarToggler.classList.remove("active");
+      navbarCollapse.classList.remove("show");
     });
   });
-  
+
+  window.addEventListener("resize", updateNavbarBrandVisibility);
+});
